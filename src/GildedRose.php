681dbc20198ad8581e9efc55690cf774e9647fa2
Tapas Datta <tapash.me@gmail.com>
2026-02-06 16:31:51 +0100

    Refactored the main GildedRose class

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,63 +4,39 @@ declare(strict_types=1);
 
 namespace GildedRose;
 
+use GildedRose\Items\AgedBrieUpdateStrategy;
+use GildedRose\Items\BackstagePassUpdateStrategy;
+use GildedRose\Items\ConjuredUpdateStrategy;
+use GildedRose\Items\DefaultUpdateStrategy;
+use GildedRose\Items\SulfurasUpdateStrategy;
+
 final class GildedRose
 {
+    private array $strategies;
+
     /**
      * @param Item[] $items
      */
     public function __construct(
         private array $items
-    ) {}
+    ) {
+        $strategies = [
+            'Aged Brie' => new AgedBrieUpdateStrategy(),
+            'Backstage passes to a TAFKAL80ETC concert' => new BackstagePassUpdateStrategy(),
+            'Sulfuras, Hand of Ragnaros' => new SulfurasUpdateStrategy(),
+            'Conjured Mana Cake' => new ConjuredUpdateStrategy(),
+            'default' => new DefaultUpdateStrategy(),
+        ];
+
+        $this->strategies = array_merge($strategies, []);
+    }
 
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+            $strategy = $this->strategies[$item->name] ?? $this->strategies['default'];
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
+            $strategy->update($item);
         }
     }
 }
